41dd200947690c9a32586516c47e607bccb44947
Alex Alexandrowitz <alexalex@mailbox.org>
2020-01-01 18:37:11 +0100

    Changed pressure from GET to POST.

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ import math
 from aiohttp import web
 
 
-async def handle(request):
-    temperature = float(request.rel_url.query["temperature"])
-    pressure = float(request.rel_url.query["pressure"])
-    height = float(request.rel_url.query["height"])
+routes = web.RouteTableDef()
+
+
+@routes.post("/pressure")
+async def pressure(request):
+    data = await request.json()
+    temperature = float(data["temperature"])
+    measured_pressure = float(data["pressure"])
+    height = float(data["height"])
     temperature_gradient = 0.0065
     temperature_kelvin = temperature + 273.15
-    result = pressure / math.pow((1-temperature_gradient*height/temperature_kelvin), (0.03416/temperature_gradient))
+    result = measured_pressure / math.pow((1-temperature_gradient*height/temperature_kelvin), (0.03416/temperature_gradient))
     response_obj = {"pressure": result}
     return web.Response(text=json.dumps(response_obj))
 
 app = web.Application()
-app.router.add_get('/pressure', handle)
+app.router.add_routes(routes)
 
 web.run_app(app)
