e8e4cde46e5f5bfd60e9ef67803f5c89bca39fd6
Alex Alexandrowitz <alexalex@mailbox.org>
2020-01-01 18:53:19 +0100

    Catch some errors and added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather-Service
+
+Small and simple service to calculate some things from weather data. Work in progress.
--- app.py
+++ app.py
@@ -10,12 +10,17 @@ routes = web.RouteTableDef()
 @routes.post("/pressure")
 async def pressure(request):
     data = await request.json()
-    temperature = float(data["temperature"])
-    measured_pressure = float(data["pressure"])
-    height = float(data["height"])
+    try:
+        temperature = float(data["temperature"])
+        measured_pressure = float(data["pressure"])
+        height = float(data["height"])
+    except ValueError:
+        raise web.HTTPInternalServerError(reason="At least one of the provided values is not valid float!")
+    except KeyError:
+        raise web.HTTPInternalServerError(reason="Please provide values for (temperature, pressure, height) in request body!")
     temperature_gradient = 0.0065
     temperature_kelvin = temperature + 273.15
-    result = measured_pressure / math.pow((1-temperature_gradient*height/temperature_kelvin), (0.03416/temperature_gradient))
+    result = measured_pressure / math.pow((1 - temperature_gradient * height / temperature_kelvin), (0.03416 / temperature_gradient))
     response_obj = {"pressure": result}
     return web.Response(text=json.dumps(response_obj))
 
